4fef539433fd859f778660857c0600701ec3cc3f
ethanl267 <71071509+ethanl267@users.noreply.github.com>
2025-07-17 12:36:14 +0200

    Update GameDataService.cs

--- Services/GameDataService.cs
+++ Services/GameDataService.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace MauiGameLibrary.Services
 {
-    internal class GameDataService
+    public class GameDataService
+{
+    private List<GameInfo> _gameInfo = new List<GameInfo>();
+
+    public List<GameInfo> GetAllGameInfo()
     {
+        return _gameInfo;
     }
+
+    public void UpdateGameInfo(GameInfo gameInfo)
+    {
+         var uniqueGame = GetGameInfoById(gameInfo.Id);
+
+
+    }
+
+    public GameInfo GetGameInfoById(int id)
+    {
+       var uniqueGame = _gameInfo.Where(x => x.Id == id).FirstOrDefault();
+
+        return uniqueGame;
+    }
+    public List<GameInfo> GetGameInfoByTitle(string title)
+    {
+        return _gameInfo.Where(x => x.Title == title).ToList();
+    }
+
+    
+}
 }
